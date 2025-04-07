dda0d6cd8c2e4bad47984e01e836b2d740ae0b58
default <yunus.emre.guzel@mths.ca>
2025-04-07 22:31:10 +0000

    added tempolete and intial change

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Emre Guzel
+// Created on: April 2 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function volumeOfSphere() {
+  // Setting the varibles
+  const radiusOfTheSphere= parseFloat(document.getElementById("radius-of-the-sphere").value)
+  const volumeOfSphere = (4/3) * Math.PI * Math.pow(radiusOfTheSphere,3)
+  // clacualtoins
+  document.getElementById("Volume").innerHTML =
+  "Volume is: " + volumeOfSphere.toFixed(2)
+}
